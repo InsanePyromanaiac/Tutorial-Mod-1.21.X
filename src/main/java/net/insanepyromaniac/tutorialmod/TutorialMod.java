00295f4d1131777214087e6d6fcd80c9b9550832
InsanePyromaniac <kacperboy2008@gmail.com>
2024-08-15 19:58:52 +0100

    Initial commit

---
+++ src/main/java/net/insanepyromaniac/tutorialmod/TutorialMod.java
@@ -0,0 +1,15 @@
+package net.insanepyromaniac.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static String ModID = ("tutorialmod");
+    public static final Logger LOGGER = LoggerFactory.getLogger(ModID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
