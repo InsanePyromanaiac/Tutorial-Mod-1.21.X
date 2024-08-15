41c101c39bce015028555272ee2e0cb2f353ab85
InsanePyromaniac <kacperboy2008@gmail.com>
2024-08-15 22:58:45 +0100

    Added 2 generic blocks

--- src/main/java/net/insanepyromaniac/tutorialmod/TutorialMod.java
+++ src/main/java/net/insanepyromaniac/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.insanepyromaniac.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.insanepyromaniac.tutorialmod.block.ModBlocks;
 import net.insanepyromaniac.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		//register items and blocks
+		ModBlocks.registerModblocks();
 		ModItems.registerModitems();
 	}
 }
