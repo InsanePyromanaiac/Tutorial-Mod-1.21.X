f2b71ae1d529083d295b2cd0a2653786fba8103a
InsanePyromaniac <kacperboy2008@gmail.com>
2024-08-18 20:13:59 +0200

    added Item and Block Groups

--- src/main/java/net/insanepyromaniac/tutorialmod/TutorialMod.java
+++ src/main/java/net/insanepyromaniac/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.insanepyromaniac.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.insanepyromaniac.tutorialmod.block.ModBlocks;
+import net.insanepyromaniac.tutorialmod.item.ModItemGroups;
 import net.insanepyromaniac.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +14,11 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		//register items and blocks
+		//register Item Groups
+		ModItemGroups.registerItemGroup();
+		//register blocks
 		ModBlocks.registerModblocks();
+		//register items
 		ModItems.registerModitems();
 	}
 }
